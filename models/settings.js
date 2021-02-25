ac897273d23f88198e6041d844161281945c302d
blagoku <76069950+blagoku@users.noreply.github.com>
2021-02-25 11:13:53 -0800

    model

--- models/settings.js
+++ models/settings.js
@@ -6,28 +6,8 @@ const settingSchema = mongoose.Schema({
   logsChannel: String,
   modRole: String,
   adminRole: String,
-  joinrole: String,
-  antiSpam: String,
-  antiLinks: String,
-  antiInvite: String,
-  antiBad: String,
-  antiEveryone: String,
-  maxMentions: Number,
-  maxLines: Number,
-  ignoredRoles: Array,
-  ignoredUsers: Array,
-  ignoredChannels: Array,
-  checkpoint: String,
-  checkpoint_logChannel: String,
-  checkpoint_mode: String,
-  underAttack: String,
-  nsfwDetection: String,
-  tags: Array,
-  selfroles: Array,
-  punishments: Array,
-  chatLogs: String,
-  moderationLogs: String,
-  serverLogs: String
+  disabledCommands: Array,
+  ignoredUsers: Array
 });
 
 module.exports = mongoose.model("settings", settingSchema);
